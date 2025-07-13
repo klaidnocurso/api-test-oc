77a4c03bc52400b953e816d8bb2809d9381b7e87
solamente <sssolammm@hotmail.com>
2025-07-13 16:20:53 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from OpenShift!' });
+  const podName = process.env.HOSTNAME;
+  console.log(`Request handled by pod: ${podName}`);
+  res.json({ message: `Hello from pod ${podName}` });
 });
 
 app.listen(port, () => {
